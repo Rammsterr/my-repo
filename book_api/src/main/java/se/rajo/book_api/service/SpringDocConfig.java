4bbf80c0e280f23bd2969582788efab19f7357e7
Rammsterr <94johram@gafe.molndal.se>
2025-02-13 15:37:37 +0100

    Add Spring Doc Config and OpenAi dependencies

---
+++ book_api/src/main/java/se/rajo/book_api/service/SpringDocConfig.java
@@ -0,0 +1,20 @@
+package se.rajo.book_api.service;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+
+                        .title("Book API")
+                      .version("1.0")
+                        .description("Book API for managing books")
+                );
+    }
+}
